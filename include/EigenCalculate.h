74c2a0a559c8612eb90db2e3c1890ee63c29680d
Rishabh <rishabhjiresal@gmail.com>
2019-10-31 16:35:21 -0400

    Minor Changes

--- include/EigenCalculate.h
+++ include/EigenCalculate.h
@@ -1,10 +1,4 @@
-//
-//  EigenCalculate.h
-//  SensorFusionRepo
-//
-//  Created by Rishabh Jiresal on 2019-10-31.
-//  Copyright © 2019 Rishabh Jiresal. All rights reserved.
-//
+
 
 #ifndef EigenCalculate_h
 #define EigenCalculate_h
--- src/SensorFusionAlgorithm.c
+++ src/SensorFusionAlgorithm.c
@@ -1,9 +1,2 @@
-//
-//  SensorFusionAlgorithm.c
-//  SensorFusionRepo
-//
-//  Created by Rishabh Jiresal on 2019-10-31.
-//  Copyright © 2019 Rishabh Jiresal. All rights reserved.
-//
 
 #include "SensorFusionAlgorithm.h"
